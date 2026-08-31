fdc6e5c314bcfbb23e54fcc68a798fbfb5831776
saieal10 <saiealnaik17@gmail.com>
2026-08-31 14:41:50 +0400

    Update Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,48 +1,68 @@
 import React from 'react';
+
 import AnnouncementBar from '../components/AnnouncementBar';
 import Navbar from '../components/Navbar';
 import Hero from '../components/Hero';
-import CountryMarquee from '../components/CountryMarquee';
 import Programs from '../components/Programs';
-import WhyChoose from '../components/WhyChoose';
 import FeaturedCountries from '../components/FeaturedCountries';
-import ManagementCountries from '../components/ManagementCountries';
-import ItalySpotlight from '../components/ItalySpotlight';
-import FeeCalculator from '../components/FeeCalculator';
-import Countries from '../components/Countries';
-import AboutUs from '../components/AboutUs';
-import Comparison from '../components/Comparison';
-import Blogs from '../components/Blogs';
-import Offices from '../components/Offices';
-import FAQ from '../components/FAQ';
 import CTABanner from '../components/CTABanner';
-import Newsletter from '../components/Newsletter';
 import Footer from '../components/Footer';
 import AiChatWidget from '../components/AiChatWidget';
 
+import MediaHub from '../components/MediaHub';
+
+
 export default function Home() {
+
   return (
+
     <div className="min-h-screen bg-cream text-ink">
+
+      {/* TOP */}
+
       <AnnouncementBar />
+
       <Navbar />
+
+
+      {/* 1. HERO */}
+
       <Hero />
-      <CountryMarquee />
+
+
+      {/* 2. TWO STUDY ROUTES
+          Medical Education + Management
+      */}
+
       <Programs />
-      <WhyChoose />
+
+
+      {/* 3. POPULAR / FEATURED DESTINATIONS */}
+
       <FeaturedCountries />
-      <ManagementCountries />
-      <ItalySpotlight />
-      <FeeCalculator />
-      <Countries />
-      <AboutUs />
-      <Comparison />
-      <Blogs />
-      <Offices />
-      <FAQ />
+
+
+      {/* 4. YOUTUBE + TESTIMONIALS */}
+
+      <MediaHub />
+
+
+      {/* 5. FINAL CTA */}
+
       <CTABanner />
-      <Newsletter />
+
+
+      {/* FOOTER */}
+
       <Footer />
+
+
+      {/* FLOATING AI GUIDE */}
+
       <AiChatWidget />
+
     </div>
+
   );
+
 }
